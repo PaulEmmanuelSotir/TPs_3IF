be25f949876c1015d943c8d97b8f9806c49f2190
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2015-10-15 01:39:45 +0200

    Modified test functions in order to take into account tests output (expected output strings still need to be filled)

--- TP1_cpp/main.cpp
+++ TP1_cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream> // used for std::cout stream redirection
 
 #include "collection.h"
 
@@ -13,7 +14,7 @@
 #pragma GCC optimize ("O0")
 #endif
 
-static void test_lifetime()
+static const char* test_lifetime()
 {
 	dog * dogsArray[3] = { new dog(color::GREEN, 50),
 		new dog(color::RED, 3),
@@ -26,6 +27,9 @@ static void test_lifetime()
 
 	volatile collection* dogs3 = new collection(100);
 	delete dogs3;
+
+	// Return expected output
+	return "";
 }
 
 #ifdef __GNUC__
@@ -36,7 +40,7 @@ static void test_lifetime()
 #pragma GCC pop_options // TODO: verifier que ça compile avec mingw et ajouter mingw64
 #endif
 
-static void test_afficher()
+static const char* test_afficher()
 {
 	dog * dogsArray[3] = { new dog(color::GREEN, 50),
 						new dog(color::RED, 3),
@@ -47,9 +51,12 @@ static void test_afficher()
 	dogs1.afficher();
 	std::cout << std::endl;
 	dogs2.afficher();
+
+	// Return expected output
+	return "";
 }
 
-static void test_ajouter()
+static const char* test_ajouter()
 {
 	dog * dogsArray[3] = { new dog(color::GREEN, 50),
 		new dog(color::RED, 3),
@@ -58,54 +65,110 @@ static void test_ajouter()
 	collection dogs1(dogsArray, 3);
 	dogs1.ajouter(new dog(color::GREEN, 5));
 	dogs1.afficher();
+	std::cout << std::endl;
 
 	collection dogs2(0);
 	dogs2.ajouter(new dog(color::GREEN, 5));
 	dogs2.afficher();
+	std::cout << std::endl;
 
 	collection dogs3(2);
 	dogs3.ajouter(new dog(color::GREEN, 5));
 	dogs3.ajouter(nullptr);
 	dogs3.afficher();
+	std::cout << std::endl;
+
+	// Return expected output
+	return "";
 }
 
-static void test_retirer()
+static const char* test_retirer()
 {
 
+	// Return expected output
+	return "";
 }
 
-static void test_ajuster()
+static const char* test_ajuster()
 {
 
+	// Return expected output
+	return "";
 }
 
-static void test_reunir()
+static const char* test_reunir()
 {
 
+	// Return expected output
+	return "";
 }
 
+/*
+* String comparition helper function
+*/
+static int string_cmp(const char* lhs, const char* rhs)
+{
+	const unsigned char *str1 = (const unsigned char *)lhs;
+	const unsigned char *str2 = (const unsigned char *)rhs;
+
+	while (*str1 != '\0') {
+		if (*str2 == '\0') return  1;
+		if (*str2 > *str1) return -1;
+		if (*str1 > *str2) return  1;
+
+		++str1;
+		++str2;
+	}
+
+	// Return -1 if 'str2' has more characters than 'str1'
+	if (*str2 != '\0')
+		return -1;
+
+	return 0;
+}
 
 /*
 * Testing helper function
 */
-static void test(void(*testFunc)(), const char* testName = "")
+static void test(const char*(*testFunc)(), const char* testName = "")
 {
 	static unsigned test_count = 0;
 
 	if (testName != "" || testName == nullptr)
 		std::cout << std::endl << std::endl << "### TEST " << ++test_count << " ###" << std::endl;
-	else 
+	else
 		std::cout << std::endl << std::endl << "### TEST " << ++test_count << " (" << testName << ") ###" << std::endl;
 
-	try
-	{
-		testFunc();
+	// Create a std::stringstream to redirect std::cout output
+	std::stringstream buffer;
+	std::streambuf * old = std::cout.rdbuf(buffer.rdbuf());
+
+	const char* expected_str;
+
+	try {
+		// Execute test function and get expected output
+		expected_str = testFunc();
+	}
+	catch (...) {
+		// Restore previous std::cout output buffer
+		std::cout.rdbuf(old);
+
+		std::cout << "FAILED : throwed an exception";
+		return;
+	}
+
+	// Get real test output
+	std::string str = buffer.str();
+	const char* output = str.data();
+
+	// Restore previous std::cout output buffer
+	std::cout.rdbuf(old);
+
+	// See if test output is as expected
+	if (string_cmp(output, expected_str) == 0)
 		std::cout << "PASSED";
-	}
-	catch (...) // can't use std::exception :(
-	{
-		std::cout << "FAILED";
-	}
+	else
+		std::cout << "FAILED : wrong output (\tEXPECTED =\t\"" << expected_str << "\",\n\t\t\tOUTPUT =\t\"" << output << "\")" << std::endl;
 }
 
 void main()
