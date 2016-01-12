80bdc626f310434dc35a982ec07c814b34cba8ea
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2016-01-12 16:07:48 +0100

    Created 'Point' alias in TP4_cpp

--- TP4_cpp/includes/Utils.h
+++ TP4_cpp/includes/Utils.h
@@ -6,11 +6,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <utility>
+
 //! \namespace TP4
 //! espace de nommage regroupant le code crée pour le TP4 de C++
 namespace TP4
 {
-	
+	using Point = std::pair<double, double>;
 }
 
 #endif // UTILS_H
