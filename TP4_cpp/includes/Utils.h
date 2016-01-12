ae7788c7f01cad2184b7e1bf7e5f24c0dfb16b10
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2016-01-12 17:23:57 +0100

    Created main function of TP4_cpp (still some implementation todo)

--- TP4_cpp/includes/Utils.h
+++ TP4_cpp/includes/Utils.h
@@ -7,12 +7,26 @@
 #define UTILS_H
 
 #include <utility>
+#include <sstream>
+#include <string>
+#include <vector>
 
 //! \namespace TP4
 //! espace de nommage regroupant le code crée pour le TP4 de C++
 namespace TP4
 {
 	using Point = std::pair<double, double>;
+
+	inline std::vector<std::string> split(const std::string &s, char delim) {
+		std::stringstream ss(s);
+		std::string item;
+		std::vector<std::string> tokens;
+
+		while (getline(ss, item, delim))
+			tokens.push_back(item);
+
+		return tokens;
+	}
 }
 
 #endif // UTILS_H
--- TP4_cpp/sources/main.cpp
+++ TP4_cpp/sources/main.cpp
@@ -1,8 +1,63 @@
+/*********************************************************************************
+									main.cpp
+									--------
+*********************************************************************************/
+
 #include <iostream>
+#include <rectangle.h>
+#include <string>
+
+#include "Utils.h"
+
+//----------------------------------------------------------------- Enumération des commandes
+// Equivalent à 'enum class cmd : std::string {...};' (underlying type non intégral)
+enum class cmd { ADD_SEGMENT, ADD_RECTANGLE, ADD_POLYGON, UNION, INTER, HIT, DELETE, MOVE, LIST, UNDO, REDO, LOAD, SAVE, CLEAR, EXIT };
+namespace { // Namespace anonyme permettant de rendre 'CMDS' et 'cmd_ut' locals
+	using cmd_ut = std::underlying_type<cmd>::type;
+	const std::string CMDS[] = {	std::string("S"), std::string("R"), std::string("PC"), std::string("OR"), std::string("OI"), std::string("HIT"), std::string("DELETE"), std::string("MOVE"), 
+									std::string("LIST"), std::string("UNDO"), std::string("REDO"), std::string("LOAD"), std::string("SAVE"), std::string("CLEAR") , std::string("EXIT") };
+}
+std::string underlying(cmd command) { return CMDS[static_cast<cmd_ut>(command)]; }
+cmd overlying(const std::string& name) {
+	for (cmd_ut i = 0; i <= static_cast<cmd_ut>(cmd::EXIT); ++i)
+	{
+		if (name == CMDS[i])
+			return static_cast<cmd>(i);
+	}
+	throw std::range_error("Given string out of 'cmd' enumeration range.");
+}
 
 int main()
 {
-	
+	while(true)
+	{
+		std::string line;
+		std::getline(std::cin, line);
 
-	return EXIT_SUCCESS;
+		auto words = TP4::split(line, ' ');
+
+		if(words.size() > 0)
+		{
+			switch (overlying(words[0]))
+			{
+			case cmd::ADD_SEGMENT: break;
+			case cmd::ADD_RECTANGLE: break;
+			case cmd::ADD_POLYGON: break;
+			case cmd::UNION: break;
+			case cmd::INTER: break;
+			case cmd::HIT: break;
+			case cmd::DELETE: break;
+			case cmd::MOVE: break;
+			case cmd::LIST: break;
+			case cmd::UNDO: break;
+			case cmd::REDO: break;
+			case cmd::LOAD: break;
+			case cmd::SAVE: break;
+			case cmd::CLEAR: break;
+			case cmd::EXIT:
+				return EXIT_SUCCESS;;
+			default: break;
+			}
+		}
+	}
 }
