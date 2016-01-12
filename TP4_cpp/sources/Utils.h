242694e52833f87d6e1bf05afe6eaba8f14ec341
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2016-01-12 12:57:39 +0100

    Created TP4_cpp (with generic makefile, empty main.cpp and Utils.h)

---
+++ TP4_cpp/sources/Utils.h
@@ -0,0 +1,17 @@
+/*********************************************************************************
+					Utils  -  A few common functions or types
+					-----------------------------------------
+*********************************************************************************/
+
+
+#ifndef UTILS_H
+#define UTILS_H
+
+//! \namespace TP4
+//! espace de nommage regroupant le code crée pour le TP4 de C++
+namespace TP4
+{
+	
+}
+
+#endif // UTILS_H
