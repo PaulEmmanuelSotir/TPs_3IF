eaf39158aa3dbf73c468ef0857a71c4fdf849d1a
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2016-01-19 14:34:16 +0100

    Added UNLIKELY and mod functions to Utils.h in TP4_cpp

--- TP4_cpp/includes/Utils.h
+++ TP4_cpp/includes/Utils.h
@@ -11,6 +11,15 @@
 #include <string>
 #include <vector>
 
+// On utilise la fonction intrinsèque __builtin_expect si disponnible
+#ifdef __GNUC__
+#define LIKELY(expr)    __builtin_expect((bool)(expr), !0)
+#define UNLIKELY(expr)  __builtin_expect((bool)(expr), 0)
+#else
+#define LIKELY(expr)    expr
+#define UNLIKELY(expr)  expr
+#endif
+
 //! \namespace TP4
 //! espace de nommage regroupant le code crée pour le TP4 de C++
 namespace TP4
@@ -26,6 +35,27 @@ namespace TP4
 			tokens.push_back(item);
 
 		return tokens;
+	}
+
+	// TODO: see if it would be better to create a struct binary op + see if handle unsigned values is needed
+	template <typename T, typename U, typename = typename _STD enable_if_t<!_STD is_arithmetic<T>::value>>
+	inline U mod(T a, T b) {
+		return a % b;
+	}
+
+	template <typename UnsignedInteger, typename = typename _STD enable_if_t<_STD is_integral<UnsignedInteger>::value && _STD is_unsigned<UnsignedInteger>::value>>
+	constexpr UnsignedInteger mod(UnsignedInteger a, UnsignedInteger b) {
+		return UNLIKELY(a >= b) ? a % b : a;
+	}
+
+	template <typename Integer, typename = typename _STD enable_if_t<_STD is_integral<Integer>::value && _STD is_signed<Integer>::value>, typename = void>
+	constexpr Integer mod(Integer a, Integer b) {
+		return UNLIKELY(a >= b || -a >= b) ? a % b : a; // see https://www.youtube.com/watch?v=nXaxk27zwlk&feature=youtu.be&t=3394
+	}
+
+	template <typename Floating, typename = typename _STD enable_if_t<_STD is_floating_point<Floating>::value>, typename = void, typename = void>
+	inline Floating mod(Floating a, Floating b) {
+		return _STD fmod(a, b); // TODO: benchmark this to see if "UNLIKELY(a >= b || -a >= b) ? ... : a;" could help.
 	}
 }
 
